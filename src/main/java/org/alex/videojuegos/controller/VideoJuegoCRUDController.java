608abcf31baf113a88e07502481ac3a6abc5ee0f
Alejandro Dimitri Bakker <alejandrodimitri.bakker@ottogroup.com>
2023-11-29 12:46:02 +0100

    Add PostMapping

--- src/main/java/org/alex/videojuegos/controller/VideoJuegoCRUDController.java
+++ src/main/java/org/alex/videojuegos/controller/VideoJuegoCRUDController.java
@@ -1,8 +1,10 @@
 package org.alex.videojuegos.controller;
 
+import org.alex.videojuegos.domain.Videojuego;
 import org.alex.videojuegos.service.DistribuidorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,6 +19,13 @@ public class VideoJuegoCRUDController {
     @RequestMapping("/videojuego/crear")
     public String mostrarFormAlta(Model model) {
         model.addAttribute("distribuidores", distribuidorService.buscarTodos());
+        model.addAttribute("videojuego", new Videojuego());
         return "formVideoJuego";
+    }
+
+    @PostMapping("/videojuego/guardar")
+    public String guardar(Videojuego videojuego) {
+        System.out.println(videojuego);
+        return "redirect:/";
     }
 }
